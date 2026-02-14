fa4814fb56859d647a0666f15818e2dbecbd2e92
sayuj63 <sayujpillai63@gmail.com>
2026-02-14 14:51:09 +0530

    feat: Add Valentine's loading animation and cursor trail

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import BehavioralPopups from "./components/BehavioralPopups";
+import ValentineFeatures from "./components/ValentineFeatures";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${montserrat.variable} ${roboto.variable} antialiased font-body overflow-x-hidden flex flex-col min-h-screen`}
       >
         <BehavioralPopups />
+        <ValentineFeatures />
         <Navbar />
         {children}
         <Footer />
